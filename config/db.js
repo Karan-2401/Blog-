6294216b1632ded81fe80a72638781be87f3323c
Karan Yadav <karan342772@gmail.com>
2025-01-25 00:39:22 +0530

    changes in db.js

--- config/db.js
+++ config/db.js
@@ -22,8 +22,7 @@ const connectDB = async () => {
 
   try {
     const db = await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
     });
     isConnected = db.connections[0].readyState;
     console.log('MongoDB connected');
