ba398b0efe83d1a2d886fa3ae6cc62ab902965da
Karan Yadav <karan342772@gmail.com>
2025-01-25 00:44:41 +0530

    changes in db.js

--- config/db.js
+++ config/db.js
@@ -12,22 +12,13 @@
 
 const mongoose = require('mongoose');
 
-let isConnected;
-
 const connectDB = async () => {
-  if (isConnected) {
-    console.log('Using existing MongoDB connection');
-    return;
-  }
-
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URL, {
-      
-    });
-    isConnected = db.connections[0].readyState;
-    console.log('MongoDB connected');
+    // Use only the MongoDB connection string
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error('Error connecting to MongoDB:', error);
     throw error;
   }
 };
