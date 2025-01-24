f74dc24d3b125e834f6f739489bda7fe4064c8a1
Karan Yadav <karan342772@gmail.com>
2025-01-25 00:35:21 +0530

    change in db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,36 @@
-const moongoose = require("mongoose");
-const connectDB = async ()=>{
-  try{
-    moongoose.set('strictQuery',false);
-    const con = await moongoose.connect(process.env.MONGODB_URL);
-    console.log(`Database has connect: ${con.connection.host}`)
-  }catch(error){
-    console.log(error)
+// const moongoose = require("mongoose");
+// const connectDB = async ()=>{
+//   try{
+//     moongoose.set('strictQuery',false);
+//     const con = await moongoose.connect(process.env.MONGODB_URL);
+//     console.log(`Database has connect: ${con.connection.host}`)
+//   }catch(error){
+//     console.log(error)
+//   }
+// }
+// module.exports = connectDB;
+
+const mongoose = require('mongoose');
+
+let isConnected;
+
+const connectDB = async () => {
+  if (isConnected) {
+    console.log('Using existing MongoDB connection');
+    return;
   }
-}
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error;
+  }
+};
+
 module.exports = connectDB;
